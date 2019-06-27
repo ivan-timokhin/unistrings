2acd6b0f5d8a6e08da1ca5e0e232ab7f122151cb
Ivan Timokhin <timokhin.iv@gmail.com>
2019-06-27 22:21:00 +0300

    Add a simple trie generator
    Of note, I have only confirmed that the results look roughly correct
    and nothing bottoms out; there are no tests in place, so no guarantees
    that the code is actually correct.
    
    I intend to add a more detailed description in comments at a later
    time, but for the time being, the central idea is to
    
    1. Partition a code point (which is a 21-bit number) into bit ranges.
    Since there are *17* planes, and actually a code point is a number in
    a range 0..0x110000, most senior 5 bits are sort of stuck together,
    and largest possible partitioning is actually at 16 bits (i.e. plane
    level).
    
    2. Represent each Unicode property table as a trie over said
    partitioning.  The partitioning can, in principle, be different for
    each property.
    
    3. Deduplicate identical sub-tries to save space.
    
    The mkTrie function in this commit does roughly this.  Full version
    will have to accommodate generation of multiple tries and choosing the
    most compact one, but this is the prototype.

--- gen/Main.hs
+++ gen/Main.hs
@@ -1,18 +1,31 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Main where
 
 import Control.Applicative ((<|>), many, optional)
+import Control.Arrow (first, second)
 import Control.Monad (guard)
+import qualified Control.Monad.Trans.State.Strict as S
 import qualified Data.Attoparsec.ByteString.Char8 as A
+import Data.Bits (shiftL)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Char as C
 import Data.Foldable (for_)
 import Data.Functor (void)
+import Data.Functor.Compose (Compose(Compose))
+import Data.Functor.Identity (Identity(Identity))
 import Data.List (sortOn)
+import qualified Data.Map.Strict as M
+import Data.Maybe (fromMaybe)
 import Data.Ord (Down(Down))
 import Data.Ratio (Rational, (%))
+import Data.Traversable (for)
+import Data.Tuple (swap)
+import qualified Data.Vector as V
 import Data.Word (Word32, Word8)
 
 main :: IO ()
@@ -30,6 +43,74 @@ unidata = do
       case rangeify records of
         Left err -> fail err
         Right ranges -> pure ranges
+
+data TrieDesc t a
+  = Bottom (t (V.Vector a))
+  | Layer Int (t (V.Vector Int)) (TrieDesc V.Vector a)
+
+deriving instance
+         (Show (t (V.Vector Int)), Show (t (V.Vector a)), Show a) =>
+         Show (TrieDesc t a)
+
+mkTrie :: Ord a => V.Vector a -> [Int] -> TrieDesc Identity a
+mkTrie xs [] = Bottom (Identity xs)
+mkTrie xs (lowBits:rest) = split (go rest) lowBits (Identity xs)
+  where
+    go [] = Bottom
+    go (lb:lbs) = split (go lbs) lb
+
+split ::
+     (Traversable t, Ord a)
+  => (V.Vector (V.Vector a) -> TrieDesc V.Vector a)
+  -> Int
+  -> t (V.Vector a)
+  -> TrieDesc t a
+split recur lowBits xs = Layer lowBits indices $ recur compressed
+  where
+    (Compose indices, compressed) =
+      deduplicate $ Compose $ fmap (matricise lowBits) xs
+
+matricise :: Int -> V.Vector a -> V.Vector (V.Vector a)
+matricise lowBits xs
+  | remainder /= 0 =
+    error "matricise: row size does not evenly divide full size"
+  | otherwise =
+    V.generate columnSize $ \rowIdx -> V.slice (rowIdx * rowSize) rowSize xs
+  where
+    fullSize = V.length xs
+    rowSize = 1 `shiftL` lowBits
+    (columnSize, remainder) = fullSize `divMod` rowSize
+
+deduplicate :: (Traversable t, Ord a) => t a -> (t Int, V.Vector a)
+deduplicate xs =
+  second invertMap . flip S.runState M.empty . for xs $ \x -> do
+    assocMap <- S.get
+    let mapSize = M.size assocMap
+        (index, newMap) =
+          first (fromMaybe mapSize) $
+          M.insertLookupWithKey (\_ _ old -> old) x mapSize assocMap
+    S.put newMap
+    pure index
+  where
+    invertMap :: M.Map a Int -> V.Vector a
+    invertMap invMap =
+      V.replicate n (error "deduplicate: element left unfilled") V.//
+      map swap (M.assocs invMap)
+      where
+        n = M.size invMap
+
+generalCategoryVector :: [UnicodeDataRange] -> V.Vector C.GeneralCategory
+generalCategoryVector ranges =
+  V.replicate unicodeTableSize C.NotAssigned V.// assignments
+  where
+    assignments =
+      ranges >>= \case
+        Single code _ udata -> [(fromIntegral code, udCategory udata)]
+        Range lo hi _ udata ->
+          [(fromIntegral i, udCategory udata) | i <- [lo .. hi]]
+
+unicodeTableSize :: Int
+unicodeTableSize = 0x110000
 
 data UnicodeDataRange
   = Single Word32 UnicodeName UnicodeData
