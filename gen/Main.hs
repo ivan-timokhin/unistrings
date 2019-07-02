a72ab4e2c3afe076c3ba57f718bcc416a2c4f277
Ivan Timokhin <timokhin.iv@gmail.com>
2019-07-02 22:25:44 +0300

    Process separate properties in parallel
    This, together with the rtsopts change, brings the running time down
    from ~47s to ~17s.  The speedup is much more than linear (x2 in this
    case), which makes me think that bumping the size of the nursery is
    generally useful in this case.

--- gen/Main.hs
+++ gen/Main.hs
@@ -5,6 +5,7 @@
 
 module Main where
 
+import Control.Concurrent.Async (mapConcurrently_)
 import Data.Char (GeneralCategory(NotAssigned))
 import Data.Foldable (for_)
 import System.Directory (createDirectoryIfMissing)
@@ -18,12 +19,14 @@ main = do
   createDirectoryIfMissing True "generated/cbits"
   createDirectoryIfMissing True "generated/hs/Data/UCD/Internal"
   createDirectoryIfMissing True "generated/test_data"
-  processTable "general_category" $
-    UCD.UnicodeData.tableToVector NotAssigned $
-    fmap UCD.UnicodeData.propCategory records
-  processTable "canonical_combining_class" $
-    UCD.UnicodeData.tableToVector 0 $
-    fmap UCD.UnicodeData.propCanonicalCombiningClass records
+  mapConcurrently_ id $
+    [ processTable "general_category" $
+      UCD.UnicodeData.tableToVector NotAssigned $
+      fmap UCD.UnicodeData.propCategory records
+    , processTable "canonical_combining_class" $
+      UCD.UnicodeData.tableToVector 0 $
+      fmap UCD.UnicodeData.propCanonicalCombiningClass records
+    ]
 
 printLong :: Show a => [a] -> IO ()
 printLong entries
