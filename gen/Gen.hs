a5674f65e81277f2dc812d3072a64178967eabd0
Ivan Timokhin <timokhin.iv@gmail.com>
2019-07-07 19:24:30 +0300

    Make code generation logic even more generic
    This change is made as a preparation for introduction of array-valued
    properties.  As a side-effect, I have switched to a slightly different
    way of arranging C arrays; it is still ugly, but at least line width
    is bounded.

--- gen/Gen.hs
+++ gen/Gen.hs
@@ -50,7 +50,7 @@ enumSpec2IntGSpec espec =
     , igsHsType = esHsType espec
     , igsHsImports =
         [B.concat ["import ", esHsTypeModule espec, " (", esHsType espec, ")"]]
-    , igsHsConvert = "toEnum . fromEnum"
+    , igsHsConvert = "toEnum . fromEnum $ "
     , igsConvert = toInteger . fromEnum
     }
 
@@ -71,7 +71,7 @@ generateIntegral spec =
       { igsCPrefix = isCPrefix spec
       , igsHsType = isHsType spec
       , igsHsImports = []
-      , igsHsConvert = "id"
+      , igsHsConvert = ""
       , igsConvert = toInteger
       }
 
@@ -85,100 +85,103 @@ data IntGSpec a =
     }
 
 generateIntG ::
-     IntGSpec a
-  -> TrieDesc Identity IntegralType IntegralType a
-  -> Module
-generateIntG igspec trie =
-  Module
-    { moduleC = generateIntGC (igsConvert igspec) (igsCPrefix igspec) trie
-    , moduleHs = generateIntGHs igspec trie
+     IntGSpec a -> TrieDesc Identity IntegralType IntegralType a -> Module
+generateIntG = generateGeneric . intGSpec2Generic
+
+intGSpec2Generic :: IntGSpec a -> GenericSpec IntegralType a
+intGSpec2Generic ispec =
+  GSpec
+    { gsCPrefix = igsCPrefix ispec
+    , gsCExtras = const []
+    , gsHsType = igsHsType ispec
+    , gsHsImports = igsHsImports ispec
+    , gsHsFFI = []
+    , gsHsConvert = \expr -> igsHsConvert ispec <> expr
+    , gsHsIntegralType = id
+    , gsConvert = igsConvert ispec
     }
 
-generateIntGC ::
-     forall a.
-     (a -> Integer)
-  -> ByteString
-  -> TrieDesc Identity IntegralType IntegralType a
+data GenericSpec ann a =
+  GSpec
+    { gsCPrefix :: ByteString
+    , gsCExtras :: ann -> [ByteString]
+    , gsHsType :: ByteString
+    , gsHsImports :: [ByteString]
+    , gsHsFFI :: [ByteString]
+    , gsHsConvert :: ByteString -> ByteString
+    , gsHsIntegralType :: ann -> IntegralType
+    , gsConvert :: a -> Integer
+    }
+
+generateGeneric ::
+     GenericSpec ann a -> TrieDesc Identity IntegralType ann a -> Module
+generateGeneric spec trie =
+  Module
+    { moduleC = generateGenericC spec trie
+    , moduleHs = generateGenericHs spec trie
+    }
+
+generateGenericC ::
+     forall a ann.
+     GenericSpec ann a
+  -> TrieDesc Identity IntegralType ann a
   -> [ByteString]
-generateIntGC f prefix = (cHeader :) . go 0
+generateGenericC spec = (cHeader :) . go 0
   where
-    go ::
-         Foldable t
-      => Int
-      -> TrieDesc t IntegralType IntegralType a
-      -> [ByteString]
+    go :: Foldable t => Int -> TrieDesc t IntegralType ann a -> [ByteString]
     go _ (Bottom ty xs) = generateBottom ty xs
     go lv (Layer ty _ layer rest) =
-      B.concat
-        [ itC ty
-        , " const "
-        , prefix
-        , "_layer_"
-        , B.pack (show lv)
-        , "[] = {"
-        , contents
-        , "};"
-        ] :
+      renderCArray
+        (itC ty)
+        (gsCPrefix spec <> "_layer_" <> B.pack (show lv))
+        (toList (Compose layer)) $
       go (lv + 1) rest
-      where
-        contents =
-          B.intercalate ", " $ map (B.pack . show) $ toList (Compose layer)
-    generateBottom ::
-         Foldable t => IntegralType -> t (V.Vector a) -> [ByteString]
-    generateBottom ty xs =
-      [B.concat [itC ty, " const ", prefix, "_bottom[] = {", contents, "};"]]
-      where
-        contents =
-          B.intercalate ", " $ map (B.pack . show . f) $ toList (Compose xs)
+    generateBottom :: Foldable t => ann -> t (V.Vector a) -> [ByteString]
+    generateBottom ann xs =
+      renderCArray
+        (itC (gsHsIntegralType spec ann))
+        (gsCPrefix spec <> "_bottom")
+        (map (gsConvert spec) $ toList (Compose xs)) $
+      gsCExtras spec ann
 
-generateIntGHs ::
-     forall a.
-     IntGSpec a
-  -> TrieDesc Identity IntegralType IntegralType a
+generateGenericHs ::
+     forall a bottomAnnotation.
+     GenericSpec bottomAnnotation a
+  -> TrieDesc Identity IntegralType bottomAnnotation a
   -> [ByteString]
-generateIntGHs spec trie =
+generateGenericHs spec trie =
   header ++ [""] ++ foreignImports 0 trie ++ [""] ++ function
   where
     foreignImports ::
          Foldable t
       => Int
-      -> TrieDesc t IntegralType IntegralType a
+      -> TrieDesc t IntegralType bottomAnnotation a
       -> [ByteString]
     foreignImports _ (Bottom ty _) =
-      [ B.concat
-          [ "foreign import ccall \"&"
-          , igsCPrefix spec
-          , "_bottom\" bottom :: Ptr "
-          , itHaskell ty
-          ]
-      ]
+      renderHsCBinding
+        (gsCPrefix spec <> "_bottom")
+        "bottom"
+        (itHaskell (gsHsIntegralType spec ty)) :
+      gsHsFFI spec
     foreignImports lv (Layer ty _ _ rest) =
-      B.concat
-        [ "foreign import ccall \"&"
-        , igsCPrefix spec
-        , "_layer_"
-        , B.pack (show lv)
-        , "\" layer_"
-        , B.pack (show lv)
-        , " :: Ptr "
-        , itHaskell ty
-        ] :
+      renderHsCBinding
+        (gsCPrefix spec <> "_layer_" <> B.pack (show lv))
+        ("layer_" <> B.pack (show lv))
+        (itHaskell ty) :
       foreignImports (lv + 1) rest
     header =
       "import Data.UCD.Internal.Ptr (Ptr, unsafeReadPtr)" :
       "import Data.Bits ((.&.), shiftR, shiftL)" :
       "import Data.Int (Int8, Int16, Int32)" :
-      "import Data.Word (Word8, Word16)" : igsHsImports spec
+      "import Data.Word (Word8, Word16)" : gsHsImports spec
     function :: [ByteString]
     function = sig : "retrieve cp = val" : " where" : map ("  " <>) locals
       where
-        sig = B.concat ["retrieve :: Int -> ", igsHsType spec]
+        sig = B.concat ["retrieve :: Int -> ", gsHsType spec]
         locals =
           case trie of
             Bottom _ (Identity _) ->
-              [ B.concat
-                  ["val = ", igsHsConvert spec, " $ unsafeReadPtr bottom cp"]
-              ]
+              [B.concat ["val = ", gsHsConvert spec "unsafeReadPtr bottom cp"]]
             Layer _ nbits _ rest ->
               B.concat
                 [ "i0 = fromEnum $ unsafeReadPtr layer_0 $ cp `shiftR` "
@@ -188,13 +191,12 @@ generateIntGHs spec trie =
         go ::
              Int
           -> Int
-          -> TrieDesc V.Vector IntegralType IntegralType a
+          -> TrieDesc V.Vector IntegralType bottomAnnotation a
           -> [ByteString]
         go depth prevBits (Bottom _ _) =
           [ B.concat
               [ "val = "
-              , igsHsConvert spec
-              , " $ unsafeReadPtr bottom $ i"
+              , gsHsConvert spec "unsafeReadPtr bottom $ i"
               , B.pack $ show depth
               , " `shiftL` "
               , B.pack $ show prevBits
@@ -218,6 +220,26 @@ generateIntGHs spec trie =
             , mask (prevBits - bits)
             ] :
           go (depth + 1) bits rest
+
+renderCArray ::
+     Show a => ByteString -> ByteString -> [a] -> [ByteString] -> [ByteString]
+renderCArray ty name [] = (B.concat [ty, " const ", name, "[] = {};"] :)
+renderCArray ty name [x] =
+  (B.concat [ty, " const ", name, "[] = { ", B.pack (show x), " };"] :)
+renderCArray ty name (x:xs) =
+  (B.concat [ty, " const ", name, "[] = { ", B.pack (show x)] :) .
+  (map (("  " <>) . foldMap (\x' -> ", " <> B.pack (show x'))) (chunksBy 10 xs) ++) .
+  ("};" :)
+
+renderHsCBinding :: ByteString -> ByteString -> ByteString -> ByteString
+renderHsCBinding cname name hsType =
+  B.concat ["foreign import ccall \"&", cname, "\" ", name, " :: Ptr ", hsType]
+
+chunksBy :: Int -> [a] -> [[a]]
+chunksBy _ [] = []
+chunksBy n xs = chunk : chunksBy n rest
+  where
+    (chunk, rest) = splitAt n xs
 
 cHeader :: ByteString
 cHeader = "#include \"HsFFI.h\"\n"
