5806a84ce009fe4e7093c8125172a8ed75630ded
Ivan Timokhin <timokhin.iv@gmail.com>
2019-07-08 12:55:22 +0300

    Switch to Attoparsec for reading test files
    This brings testing time down from almost a minute to a couple of
    seconds — a nice quality of life improvement!

--- test/Main.hs
+++ test/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
-import Control.Monad (when)
+import Control.Monad (replicateM, when)
+import qualified Data.Attoparsec.ByteString.Char8 as A
+import qualified Data.ByteString.Char8 as B
 import Data.Foldable (for_)
+import Data.List (sortOn)
+import Data.Ord (Down(Down))
 import System.Exit (exitFailure)
 import Test.HUnit
 
@@ -17,8 +21,7 @@ generalCategory :: Test
 generalCategory =
   TestLabel "General category" $
   TestCase $ do
-    reference <-
-      map read . lines <$> readFile "generated/test_data/general_category.txt"
+    reference <- readFullTable enumP "generated/test_data/general_category.txt"
     assertEqual "Table size" (fromEnum maxCp - fromEnum minCp + 1) $
       length reference
     for_ (zip [minCp .. maxCp] reference) $ \(cp, refGC) ->
@@ -29,8 +32,9 @@ canonicalCombiningClass =
   TestLabel "Canonical combining class" $
   TestCase $ do
     reference <-
-      map read . lines <$>
-      readFile "generated/test_data/canonical_combining_class.txt"
+      readFullTable
+        A.decimal
+        "generated/test_data/canonical_combining_class.txt"
     assertEqual "Table size" (fromEnum maxCp - fromEnum minCp + 1) $
       length reference
     for_ (zip [minCp .. maxCp] reference) $ \(cp, refCCC) ->
@@ -41,3 +45,18 @@ maxCp = maxBound
 
 minCp :: UCD.CodePoint
 minCp = minBound
+
+readFullTable :: A.Parser a -> FilePath -> IO [a]
+readFullTable p file = do
+  txt <- B.readFile file
+  let parsed = A.parseOnly (replicateM 0x110000 (p <* A.char '\n')) txt
+  case parsed of
+    Left err -> assertFailure err
+    Right vals -> pure vals
+
+enumP :: (Enum a, Show a, Bounded a) => A.Parser a
+enumP =
+  A.choice $
+  map (\(e, str) -> e <$ A.string str) $
+  sortOn (Down . B.length . snd) $
+  map (\e -> (e, B.pack (show e))) [minBound .. maxBound]
