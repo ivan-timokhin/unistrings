5ca37e98eb847bff13afc628a7d8d1e40ae40fef
Ivan Timokhin <timokhin.iv@gmail.com>
2020-01-18 00:00:19 +0300

    Add a small compatibility module for indexed TypeRep
    It's not publicly exposed, since this sort of compatibility
    shenanigans are exclusively private matter.
    
    Speaking of which, the only reason why I even need it is because I've
    decided to support GHC 8.0 and base-4.9 that comes with it.  Given
    that we're nearing a release of GHC 8.10, is that /necessary/?  I
    don't know, but it's here for the time being.

---
+++ unistring/src/Data/Unistring/Compat/Typeable.hs
@@ -0,0 +1,54 @@
+{-
+Copyright 2020 Ivan Timokhin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_HADDOCK show-extensions #-}
+{-|
+Module      : Data.Unistring.Compat.Typeable
+Description : Uniform Typeable interface for base-4.9
+Copyright   : (c) Ivan Timokhin 2020
+License     : Apache-2.0
+Maintainer  : timokhin.iv@gmail.com
+Stability   : experimental
+
+
+-}
+module Data.Unistring.Compat.Typeable
+  ( Typeable
+  , TypeRep
+  , typeRep
+  , eqTypeRep
+  ) where
+#if MIN_VERSION_base(4, 10, 0)
+import Type.Reflection (TypeRep, Typeable, eqTypeRep, typeRep)
+#else
+import Data.Typeable (Typeable, eqT)
+import Data.Type.Equality ((:~:))
+
+-- This type wraps a constraint instead of Data.Typeable.TypeRep,
+-- because TypeRep isn't indexed, but the dictionary /is/, which means
+-- there's no need to manually keep track of indices ourselves.
+data TypeRep a where
+  TypeRep :: Typeable a => TypeRep a
+
+typeRep :: Typeable a => TypeRep a
+typeRep = TypeRep
+
+eqTypeRep :: TypeRep a -> TypeRep b -> Maybe (a :~: b)
+eqTypeRep TypeRep TypeRep = eqT
+#endif
